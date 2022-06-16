83402b195394ffc5e273bcc7d348bae66d67dafb
Artem Evsyakov <evsyakov@gmail.com>
2022-06-16 15:02:57 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
+import { useState } from 'react';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const handleChange = (e) => setUrl(e.target.value);
+
+
   return (
     <>
-      <Home />
-      <Login />
-      <Settings />
+      <input value={url} onChange={handleChange} />
+      {url === '/home' && <Home />}
+      {url === '/login' && <Login />}
+      {url === '/settings' && <Settings />}
 	  </>
   );
 }
