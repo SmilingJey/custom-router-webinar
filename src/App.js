cbe1569d78b513869c026cb0e74c05605b3c73fd
Artem Evsyakov <evsyakov@gmail.com>
2022-06-16 15:07:26 +0300

    3

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 
+function Route({ path, currentPath, children }) {
+	return currentPath === path && children;
+}
+
 function App() {
   const [url, setUrl] = useState('');
   const handleChange = (e) => setUrl(e.target.value);
 
-
   return (
     <>
       <input value={url} onChange={handleChange} />
-      {url === '/home' && <Home />}
-      {url === '/login' && <Login />}
-      {url === '/settings' && <Settings />}
+      <Route path="/home" currentPath={url}><Home /></Route>
+		  <Route path="/login" currentPath={url}><Login /></Route>
+	  	<Route path="/settings" currentPath={url}><Settings /></Route>
 	  </>
   );
 }
